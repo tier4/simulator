d10a8c503ee6249a38c8de2e0947ad593e169e38
kaichi.oda <kaichi.oda@tier4.jp>
2020-09-15 23:36:10 +0900

    add agentpushmessage command

---
+++ Assets/Scripts/Api/Commands/AgentPushMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.EventSystems;
+
+namespace Simulator.Api.Commands
+{
+    public class AgentPushMessage : IDistributedCommand
+    {
+        public string Name => "simulator/push_message";
+        private SimulatorManager _sim = null;
+
+        SimulatorManager sim => _sim ?? Object.FindObjectOfType<SimulatorManager>();
+
+        public void Execute(JSONNode args)
+        {
+            var api = ApiManager.Instance;
+            Debug.Log($"AgentPushMessage.Execute()");
+            sim.AgentManager.BroadcastMessage("PushMessage", args["message"].ToString(), SendMessageOptions.DontRequireReceiver);
+
+            api.SendResult(this);
+        }
+    }
+}
